7467f6ec2ee241c88fbc094d63eeb62688f2b9df
Craig Miller <craigmiller160@gmail.com>
2016-05-21 08:49:37 -0400

    Added some commented information to shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # Shutdown script
 
+## ADD THIS TO .bashrc
+## xhost local:craig >/dev/null
+
 zenity --question --text "Automated system shutdown set to begin in 30 seconds." --ok-label="Shutdown Now" --cancel-label="Cancel Shutdown" --title "System Shutdown" --display=:0.0 --timeout 30
-
-
-# XAUTHORITY=/home/craig/.Xauthority
-
-# echo $?
-# echo $choice
 
 if [ $? -ne 1 ]; then
 	zenity --info --display=:0.0 --text "Preparing to shut down machine" --timeout 3
